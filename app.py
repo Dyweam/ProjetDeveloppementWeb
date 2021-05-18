1d3d987d7e47783579d58cb00d811507cd24dee5
Dyweam <51490460+Dyweam@users.noreply.github.com>
2021-05-18 18:14:00 +0200

    WebSite pages add
    - Ajout des pages web dont on aura besoin
    - Ajout des routes dans app.py

--- app.py
+++ app.py
@@ -3,8 +3,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index/index.html')
+    return render_template('pages/index.html')
 
 @app.route('/artists')
 def artists():
-    return render_template('artists/artists.html')
+    return render_template('pages/artists.html')
+
+@app.route('/albums')
+def albums():
+    return render_template('pages/albums.html')
+
+@app.route('/genre')
+def genre():
+    return render_template('pages/genre.html')
+
+@app.route('/clients')
+def clients():
+    return render_template('pages/clients.html')
+
+@app.route('/commands')
+def commands():
+    return render_template('pages/commands.html')
+
+@app.route('/employees')
+def employees():
+    return render_template('pages/employees.html')
