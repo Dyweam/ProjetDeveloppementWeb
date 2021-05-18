b08238fa8559189e0bc3fda48d29047e42dbfb9b
Dyweam <51490460+Dyweam@users.noreply.github.com>
2021-05-18 10:20:27 +0200

    Artists page add
    - Ajout de la page "artists", celle-ci n'est pas encore modifié

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index/index.html')
+
+@app.route('/artists')
+def artists():
+    return render_template('artists/artists.html')
